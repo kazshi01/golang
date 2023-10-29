c8f063f7b5e3ecb418d8f1d660741c2390565f15
kazshi01 <blumedio0619@gmail.com>
2023-10-30 07:27:06 +0900

    add backend

--- terraform/modules/fargate/versions.tf
+++ terraform/modules/fargate/versions.tf
@@ -11,3 +11,11 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  backend "s3" {
+    bucket = "self-dev-marukome"
+    key    = "golang/terraform"
+    region = "ap-northeast-1"
+  }
+}
