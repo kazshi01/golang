f1eee7bcf735db94168674992230c9fa68aa2799
kazshi01 <blumedio0619@gmail.com>
2023-11-23 09:00:52 +0900

    コメント

--- backend/router/router.go
+++ backend/router/router.go
@@ -26,9 +26,10 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController) *ec
 		CookiePath:     "/",
 		CookieDomain:   os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
-		// CookieSameSite: http.SameSiteNoneMode,
-		CookieSameSite: http.SameSiteDefaultMode,
-		// CookieMaxAge:   60,
+		CookieSecure:   true,                  // HTTPSでのみクッキーを送信
+		CookieSameSite: http.SameSiteNoneMode, // SameSite属性をNoneに設定
+		// CookieSameSite: http.SameSiteDefaultMode,
+		CookieMaxAge: 60, // クッキーの有効期限を設定（秒）
 	}))
 
 	// ユーザー関連のルート
