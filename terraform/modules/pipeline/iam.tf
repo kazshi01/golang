1685d865eecaf546bc3da8fe9eeee09e180fbf7c
kazshi01 <blumedio0619@gmail.com>
2024-01-04 14:16:00 +0900

    mod codedeploy iamrole

--- terraform/modules/pipeline/iam.tf
+++ terraform/modules/pipeline/iam.tf
@@ -20,25 +20,3 @@ resource "aws_iam_role_policy_attachment" "codedeploy_ecs_attachment" {
   role       = aws_iam_role.codedeploy_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
-
-# ECS Serviceの情報を取得するためのIAM Policy
-resource "aws_iam_policy" "ecs_describe_services_policy" {
-  name        = "ecs-describe-services-policy"
-  description = "Allow ecs:DescribeServices on ECS services"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = "ecs:DescribeServices"
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_describe_services_policy_attachment" {
-  role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
-}
